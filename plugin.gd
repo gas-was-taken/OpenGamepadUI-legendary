c0fcf81c036fb929898191b385357eab932150ce
Gas <166496493+gas-was-taken@users.noreply.github.com>
2025-03-03 13:20:35 +0100

    Basic library plugin for legendary (Epic Games)

--- plugin.gd
+++ plugin.gd
@@ -2,5 +2,7 @@ extends Plugin
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	logger = Log.get_logger("TemplatePlugin", Log.LEVEL.DEBUG)
-	logger.info("Template plugin loaded")
+	logger = Log.get_logger("Legendary", Log.LEVEL.DEBUG)
+	
+	var library: Library = load(plugin_base + "/core/library.tscn").instantiate()
+	add_child(library)
